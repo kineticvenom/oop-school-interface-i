333d1df55d993b9f1687e6a4c555704d381fa0b2
kineticvenom <kalebvarnes@yahoo.com>
2022-06-02 11:26:34 -1000

    Working Solution

---
+++ classes/student.py
@@ -0,0 +1,24 @@
+
+from classes.person import Person
+
+import csv
+
+class Student(Person):
+    # super(Person).__init__(self)
+    
+      
+    
+    def all_students():
+
+        individual_students = []
+
+        with open ('data/students.csv', 'r' ) as student_f :
+            rows = csv.reader(student_f, delimiter= ",")
+            value = next(rows)
+
+            for row in rows :
+                individual_students.append(Student(*row))
+        return individual_students
+
+
+
